71497a8d7d200b3f27415ab46c221c3654e5c6bf
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2022-11-10 22:20:17 +0300

    Api models added & Retrofit setup completed & Dagger - Hilt setup completed

---
+++ app/src/main/java/com/dogactnrvrdi/artbook/Application.kt
@@ -0,0 +1,7 @@
+package com.dogactnrvrdi.artbook
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application()
---
+++ app/src/main/java/com/dogactnrvrdi/artbook/util/Util.kt
@@ -0,0 +1,7 @@
+package com.dogactnrvrdi.artbook.util
+
+object Util {
+
+    const val API_KEY = "12045663-784ff9565d715f2e61cd44c27"
+    const val BASE_URL = "https://pixabay.com"
+}
